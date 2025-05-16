719cf1bfa20403e2f56004c2639c2c4367811589
Yunus Shakariya <yunussh03@gmail.com>
2025-05-16 17:35:49 +0530

    Add files via upload

---
+++ bulb on off/script.js
@@ -0,0 +1,6 @@
+function turnOn() {
+  document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulbon.gif";
+}
+function turnOff() {
+  document.getElementById("bulb").src = "https://www.w3schools.com/js/pic_bulboff.gif";
+}
